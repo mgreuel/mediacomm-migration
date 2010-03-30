434bfe81fb3a7302b147ab77a72e1332b573b349
mgreuel_cp <SND\mgreuel_cp>
2010-03-30 16:30:58 +0000

    Added WebTest

---
+++ MediaCommMVC.WebTests/Pages/Movies/MoviesIndexPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using WatiN.Core;
+
+namespace MediaCommMVC.WebTests.Pages.Movies
+{
+    public class MoviesIndexPage : LoginPage
+    {
+        [FindBy(Id = "showAddPopup")]
+        public Link ShowPopUpLink;
+
+        [FindBy(Id = "editMoviePopup")]
+        public Div editMoviePopup;
+
+        public static void GoTo(Browser browser)
+        {
+            browser.GoTo(ConfigurationManager.AppSettings["baseUrl"] + "/Movies");
+        }
+    }
+}
---
+++ MediaCommMVC.WebTests/Tests/Movies/MoviesIndexPageTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+using MediaCommMVC.WebTests.Framework;
+using MediaCommMVC.WebTests.Pages.Movies;
+
+using WatiN.Core;
+
+namespace MediaCommMVC.WebTests.Tests.Movies
+{
+    public class MoviesIndexPageTest : BrowserTestFixture
+    {
+        [FixtureSetUp]
+        public void EnsureLogin()
+        {
+            WatiN.Core.Browser browser = new IE();
+            this.LoginWithBrowser(browser);
+
+            browser = new FireFox();
+            this.LoginWithBrowser(browser);
+        }
+
+        private void LoginWithBrowser(Browser browser)
+        {
+            MoviesIndexPage.GoTo(browser);
+            var page = browser.Page<MoviesIndexPage>();
+
+            if (page.Document.Div("login").Exists)
+            {
+                page.LoginUsingDefaultUser();
+            }
+        }
+
+        [Test]
+        [Browser(BrowserType.IE)]
+        [Browser(BrowserType.FireFox)]
+        public void CanGoToPage()
+        {
+            MoviesIndexPage.GoTo(Browser);
+            var page = Browser.Page<MoviesIndexPage>();
+
+            Assert.IsTrue(page.Document.Table("movieTable").Exists);
+        }
+
+        [Test]
+        [Browser(BrowserType.IE)]
+        [Browser(BrowserType.FireFox)]
+        public void AddMoviePopIsNotDisplayed()
+        {
+            MoviesIndexPage.GoTo(Browser);
+            var page = Browser.Page<MoviesIndexPage>();
+
+            Div popupDiv = page.Document.Div("editMoviePopup");
+            Assert.IsTrue(popupDiv.GetAttributeValue("style").Contains("display: none;"));
+        }
+
+        [Test]
+        [Browser(BrowserType.IE)]
+        [Browser(BrowserType.FireFox)]
+        public void AddMoviePopIsDisplayedAfterDisplayClick()
+        {
+            MoviesIndexPage.GoTo(Browser);
+            var page = Browser.Page<MoviesIndexPage>();
+
+            page.ShowPopUpLink.Click();
+
+            Div popupDiv = page.Document.Div("editMoviePopup");
+
+            Console.WriteLine();
+        }
+    }
+}
