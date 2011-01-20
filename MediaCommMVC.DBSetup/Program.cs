acc5d780599caeed2e6b21c0ae5c659772829341
mgreuel_cp <SND\mgreuel_cp>
2011-01-20 18:26:09 +0000

    Fixed references

--- MediaCommMVC.DBSetup/Program.cs
+++ MediaCommMVC.DBSetup/Program.cs
@@ -18,7 +18,6 @@ using MediaCommMVC.Tests.Common.TestImplementations;
 using MediaCommMVC.Tests.TestImplementations;
 
 using NHibernate;
-using NHibernate.Tool.hbm2ddl;
 
 using Configuration = NHibernate.Cfg.Configuration;
 
@@ -66,8 +65,7 @@ namespace MediaCommMVC.DBSetup
             configuration.Configure();
 
             FluentConfiguration config =
-                Fluently.Configure(configuration).Mappings(m => m.AutoMappings.Add(autoMapModel)).ExposeConfiguration(
-                    BuildSchema);
+                Fluently.Configure(configuration).Mappings(m => m.AutoMappings.Add(autoMapModel));
 
             return config;
         }
@@ -138,21 +136,6 @@ namespace MediaCommMVC.DBSetup
 
         #region Methods
 
-        /// <summary>
-        /// Builds the schema in the database file.
-        /// </summary>
-        /// <param name="config">
-        /// The config.
-        /// </param>
-        private static void BuildSchema(Configuration config)
-        {
-            // Drops all tables
-            new SchemaExport(config).Drop(true, true);
-
-            // this NHibernate tool takes a configuration (with mapping info in)
-            // and exports a database schema from it
-            new SchemaExport(config).Create(true, true);
-        }
 
         #endregion
     }
