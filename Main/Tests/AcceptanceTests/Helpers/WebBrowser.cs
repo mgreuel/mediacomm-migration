a529363f2cdea3e33cf46b4df885de1605e957ca
mgreuel_cp <SND\mgreuel_cp>
2011-03-22 09:41:29 +0000

    Added test packages

---
+++ Main/Tests/AcceptanceTests/Helpers/WebBrowser.cs
@@ -0,0 +1,40 @@
+﻿namespace MediaCommMVC.Tests.AcceptanceTests.Helpers
+{
+    #region Using Directives
+
+    using System.Configuration;
+    using MvcContrib.TestHelper.WatiN;
+    using TechTalk.SpecFlow;
+    using WatiN.Core;
+
+    #endregion
+
+    public static class WebBrowser
+    {
+        public static IE Current
+        {
+            get
+            {
+                if (!ScenarioContext.Current.ContainsKey("browser"))
+                {
+                    ScenarioContext.Current["browser"] = new IE();
+                }
+
+                return (IE)ScenarioContext.Current["browser"];
+            }
+        }
+
+        public static WatinDriver Driver
+        {
+            get
+            {
+                if (!ScenarioContext.Current.ContainsKey("browserDriver"))
+                {
+                    ScenarioContext.Current["browserDriver"] = new WatinDriver(Current, ConfigurationManager.AppSettings["baseUrl"]);
+                }
+
+                return (WatinDriver)ScenarioContext.Current["browserDriver"];
+            }
+        }
+    }
+}
